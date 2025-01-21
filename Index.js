a76977b7005b9aa309a86785bfd72c08b0619598
comparini_Gabriel <comparinizk@outlook.com>
2025-01-21 07:56:28 +0100

    Creating Server APi

--- Index.js
+++ Index.js
@@ -1,1 +1,19 @@
-console.log(ola mundo)
+const express = require('express');
+
+const app = express();
+
+app.use(express.json()) 
+
+
+app.get('/players', (req, resp) => {
+
+    console.log("Get no server");
+    resp.status(200).send('deu certo');
+} )
+
+
+app.listen(3004, () => {
+
+console.log('inicio servidor da api')
+
+})
