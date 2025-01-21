41e426000d72434f913bae6bfdf9c20e6ab2da9d
comparini_Gabriel <comparinizk@outlook.com>
2025-01-21 08:27:25 +0100

    Adicionado method Get and Post API

--- Index.js
+++ Index.js
@@ -7,13 +7,28 @@ app.use(express.json())
 
 app.get('/players', (req, resp) => {
 
+    console.log(req.query)
     console.log("Get no server");
     resp.status(200).send('deu certo');
 } )
 
+app.post('/players', (req, resp) => {
+
+    console.log(req.body)
+    console.log("Get no server");
+    resp.status(200).send(req.body);
+} )
 
 app.listen(3004, () => {
 
 console.log('inicio servidor da api')
+
+})
+
+
+app.post('/players', (req, resp) => {
+
+        
+
 
 })
